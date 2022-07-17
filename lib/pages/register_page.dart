0daec39a8185a75250928a9fc8f8c83eacb63f29
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2022-07-17 20:51:04 +0200

    alert changes need to be merged

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+//alert box needs to be placed
 
 class RegisterPage extends StatefulWidget {
   final VoidCallback showLoginPage;
